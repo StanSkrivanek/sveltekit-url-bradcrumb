2b2ab42ab1b068d19c2a47558a9851018ade7bda
Stan Skrivanek <s.skrivanek@icloud.com>
2023-02-05 11:42:26 +0000

    basic backup

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -4,12 +4,14 @@
 	export let data: PageData;
 	// console.log($page);
 	const match = data?.products?.filter(({ id }) => id.toString() === $page.params.id);
-	const { title, description } = match[0] || {};
+	const { title, description, category } = match[0] || {};
 </script>
 
 <h1>Product</h1>
 
 {#if match}
 	<h2>{title}</h2>
+	<h3>{category}</h3>
 	<p>{description}</p>
+
 {/if}
